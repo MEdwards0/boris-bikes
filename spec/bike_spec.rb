9d75dd89c9d71aa131e7434ca261d5aa60c27b61
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-04-27 16:31:43 +0100

    Updated all files

---
+++ spec/bike_spec.rb
@@ -0,0 +1,7 @@
+require 'bike.rb'
+
+describe Bike do
+  it "bike responds to working" do
+    expect(subject).to respond_to :working?
+  end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,32 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  it "docking station responds to release bike" do
+  expect(subject).to respond_to :release_bike
+  end
+
+  it "gets bike and checks bike is working" do
+  bike = subject.release_bike
+  expect(bike).to be_working
+  end
+
+  it "allows a bike to be docked" do
+  expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "allows you to view bikes" do
+  expect(subject).to respond_to :bike
+  end
+
+  it "docks a bike" do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "returns if a bike is docked" do
+    bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq(bike)
+  end
 
 end
